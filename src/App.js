626257693f2b1125c8f276bf720210adcacb98ef
Ally Elvis Nzeyimana <allyelvis6569@gmail.com>
2024-11-20 23:13:04 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -23,3 +23,16 @@ function App() {
 }
 
 export default App;
+
+import React from "react";
+import Login from "./components/Login";
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
